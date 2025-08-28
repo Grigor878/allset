0d0f0ffb26862214e399fefda7cb593b948d0611
Grigor Safaryan <grigor.safaryan@ekeng.am>
2025-08-28 10:34:48 +0400

    promocode Datalist fixations

--- src/components/constructor/promocode.jsx
+++ src/components/constructor/promocode.jsx
@@ -2,6 +2,7 @@ import { t } from "i18next";
 import { useState } from "react";
 import {
   Button,
+  DataList,
   Field,
   Flex,
   Icon,
@@ -29,7 +30,7 @@ export const Promocode = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!promocode) return;
-    mutate({ promocode });
+    // mutate({ promocode });
     setApplied(true); // test case
   };
 
@@ -109,49 +110,108 @@ export const Promocode = () => {
             </Button>
           </Flex>
           {applied && !isPending && (
-            <Stack>
-              <Flex w="100%" justify={"space-between"}>
-                <Text
+            <DataList.Root
+              orientation="horizontal"
+              gap="8px"
+              // maxW="md"
+            >
+              <DataList.Item>
+                <DataList.ItemLabel
                   fontSize={"13px"}
                   color={"#4B5563"}
                   lineHeight={"20px"}
                   fontWeight={"400"}
                 >
                   {t("original_price")}
-                </Text>
-                <Text textStyle="sm" lineHeight={"20px"}>
+                </DataList.ItemLabel>
+                <DataList.ItemValue
+                  fontSize={"14px"}
+                  lineHeight={"20px"}
+                  justifyContent={"flex-end"}
+                >
                   $49.99
-                </Text>
-              </Flex>
-              <Flex w="100%" justify={"space-between"}>
-                <Text
+                </DataList.ItemValue>
+              </DataList.Item>
+              <DataList.Item>
+                <DataList.ItemLabel
                   fontSize={"13px"}
                   color={"#4B5563"}
                   lineHeight={"20px"}
                   fontWeight={"400"}
                 >
                   {t("discount")} ({discount}%):
-                </Text>
-                <Text textStyle="sm" fontWeight={"400"}>
+                </DataList.ItemLabel>
+                <DataList.ItemValue
+                  fontSize={"14px"}
+                  lineHeight={"20px"}
+                  justifyContent={"flex-end"}
+                >
                   -$10.00
-                </Text>
-              </Flex>
+                </DataList.ItemValue>
+              </DataList.Item>
               <Separator />
-              <Flex w="100%" justify={"space-between"}>
-                <Text
+              <DataList.Item w="100%">
+                <DataList.ItemLabel
                   fontSize={"13px"}
                   color={"#4B5563"}
                   lineHeight={"20px"}
                   fontWeight={"400"}
                 >
                   {t("final_price")}
-                </Text>
-                <Text>$39.99</Text>
-              </Flex>
-            </Stack>
+                </DataList.ItemLabel>
+                <DataList.ItemValue
+                  fontSize={"16px"}
+                  lineHeight={"20px"}
+                  justifyContent={"flex-end"}
+                >
+                  $39.99
+                </DataList.ItemValue>
+              </DataList.Item>
+            </DataList.Root>
           )}
         </Stack>
       </Field.Root>
     </Stack>
   );
 };
+
+// {/* <Stack>
+//   <Flex w="100%" justify={"space-between"}>
+//     <Text
+//       fontSize={"13px"}
+//       color={"#4B5563"}
+//       lineHeight={"20px"}
+//       fontWeight={"400"}
+//     >
+//       {t("original_price")}
+//     </Text>
+//     <Text textStyle="sm" lineHeight={"20px"}>
+//       $49.99
+//     </Text>
+//   </Flex>
+//   <Flex w="100%" justify={"space-between"}>
+//     <Text
+//       fontSize={"13px"}
+//       color={"#4B5563"}
+//       lineHeight={"20px"}
+//       fontWeight={"400"}
+//     >
+//       {t("discount")} ({discount}%):
+//     </Text>
+//     <Text textStyle="sm" fontWeight={"400"}>
+//       -$10.00
+//     </Text>
+//   </Flex>
+//   <Separator />
+//   <Flex w="100%" justify={"space-between"}>
+//     <Text
+//       fontSize={"13px"}
+//       color={"#4B5563"}
+//       lineHeight={"20px"}
+//       fontWeight={"400"}
+//     >
+//       {t("final_price")}
+//     </Text>
+//     <Text>$39.99</Text>
+//   </Flex>
+// </Stack> */}
